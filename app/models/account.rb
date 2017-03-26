6430ebe76790e7ad6b17180551d770630a8e1ff5
Luisk Hernandez <luis.macias@koombea.com>
2017-03-26 12:56:26 -0500

    Initial Commit

---
+++ app/models/account.rb
@@ -0,0 +1,24 @@
+class Account < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable,
+    :confirmable, :lockable
+
+  validates :domain, uniqueness: true
+  validates :subdomain, uniqueness: true
+  validates :name, uniqueness: true
+
+  after_create :create_tenant
+  after_destroy :destroy_tenant
+
+  private
+
+  def create_tenant
+    Apartment::Tenant.create(subdomain)
+  end
+
+  def destroy_tenant
+    Apartment::Tenant.drop(subdomain)
+  end
+end
