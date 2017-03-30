897f3b025faa3f23d500548a4a0b9103ba3a6b05
Luisk Hernandez <luis.macias@koombea.com>
2017-03-30 09:05:46 -0500

    Add Html template to dashboard
    Add boostrap css for that board

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  namespace :accounts do
-    get 'dashboard/index'
-  end
-
   root 'home#index'
 
   devise_for :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :accounts do
-    resources :dashboard, only: :index
+    resources :dashboard, only: [:index]
   end
 end
